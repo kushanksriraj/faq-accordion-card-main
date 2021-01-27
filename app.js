39bfa762491f68806c1c04043ebd8525baf7f5e1
Kushank Sriraj <kushanksriraj@gmail.com>
2021-01-27 14:12:17 +0530

    style: image

---
+++ app.js
@@ -0,0 +1,23 @@
+for (let i=1; i<=5; i++){
+
+    const faq = [];
+    faq[i] = document.querySelector(".faq-block-" + i);
+    faq[i].addEventListener("click", i => clickEventHandler(i));
+}
+
+
+
+function clickEventHandler(i){
+
+    
+  
+}
+
+
+
+
+
+
+
+
+
